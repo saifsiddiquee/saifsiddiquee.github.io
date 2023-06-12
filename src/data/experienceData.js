12dc5d9999d0efab1b083d1438127b2d8e3a9d16
saifsiddiquee <saif.rafi07@gmail.com>
2023-06-12 18:39:34 +0600

    Info updae

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -3,18 +3,25 @@ export const experienceData = [
         id: 1,
         company: 'Brac IT Services Ltd.',
         jobtitle: 'Sr. Software Engineer',
-        startYear: '2020',
+        startYear: '2023',
         endYear: 'Present'
     },
     {
         id: 2,
+        company: 'Brac IT Services Ltd.',
+        jobtitle: 'Software Engineer',
+        startYear: '2020',
+        endYear: '2022'
+    },
+    {
+        id: 3,
         company: 'Royalty Bangladesh Inc.',
         jobtitle: 'Junior Android Developer',
         startYear: '2019',
         endYear: '2020'
     },
     {
-        id: 3,
+        id: 4,
         company: 'Geeks\'n\'Technology Ltd.',
         jobtitle: 'Programmer Intern',
         startYear: 'October 2018',
